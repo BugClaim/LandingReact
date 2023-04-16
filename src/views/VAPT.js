c376352217921e98e3e62a34a7d5caffb7f0d022
Aakash Kumar <aakash.kumar@quicksell.co>
2023-04-16 22:48:04 +0530

    fix: updated

--- src/views/VAPT.js
+++ src/views/VAPT.js
@@ -2,9 +2,40 @@ import React from "react";
 
 const VAPT = () => {
   return (
-    <>
-      <h1>Coming Soon</h1>
-    </>
+    <div style={{ padding: "120px 120px" }}>
+      <div className="services-container">
+        <h2>Our Services</h2>
+        <div className="service">
+          <h3>Vulnerability Assessment & Penetration Testing Services</h3>
+          <p>
+            Our expert team can help you identify vulnerabilities in your
+            systems and networks, and provide recommendations for how to address
+            them.
+          </p>
+        </div>
+        <div className="service">
+          <h3>Attack Surface Monitoring (ASM)</h3>
+          <p>
+            We can help you stay ahead of potential attacks by monitoring your
+            attack surface and alerting you to any changes or vulnerabilities.
+          </p>
+        </div>
+        <div className="service">
+          <h3>Red Teaming</h3>
+          <p>
+            Our experienced red team can simulate real-world attacks to help you
+            identify weaknesses in your defenses and improve your overall
+            security posture.
+          </p>
+        </div>
+        <p className="services-footer">
+          At BugClaim, we're committed to providing high-quality services that
+          help our customers stay secure and protected in today's ever-changing
+          threat landscape. Contact us today to learn more about how we can help
+          you safeguard your organization.
+        </p>
+      </div>
+    </div>
   );
 };
 
